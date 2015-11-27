fbf70fb145d2daf6e5ca4b4495b25a2df8e48ca1
Toshihiro Yagi <toshihiro-yagi@cookpad.com>
2015-11-27 13:35:44 +0900

    Available parameter.
    ex: avd_peco "-http-proxy http://192.168.1.1:8888"

--- bin/avd_peco.sh
+++ bin/avd_peco.sh
@@ -1,5 +1,4 @@
 avd_peco(){
-
   if ! type android 2>/dev/null 1>/dev/null
   then
     echo -e 'Error: android command not found.\nYou must install Android SDK.\nhttps://developer.android.com/sdk/index.html'
@@ -12,7 +11,7 @@ avd_peco(){
 
   avd_name=`android list avds | grep -oe "Name: [a-zA-Z0-9_]*" | sed -e 's/Name: //' | peco`
   if [[ $avd_name =~ ^(.+) ]] ; then
-     echo "boot $avd_name"
-     emulator -avd "$avd_name" &
+     echo "boot $avd_name $1"
+     emulator -avd "$avd_name" $1 &
   fi
 }
