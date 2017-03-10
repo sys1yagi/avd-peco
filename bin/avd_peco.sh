7ea85c291d6f985000043c2c652106964b536be6
Toshihiro Yagi <sylc.yagi@gmail.com>
2017-03-10 13:28:35 +0900

    Update the command for latest android sdk.

--- bin/avd_peco.sh
+++ bin/avd_peco.sh
@@ -1,5 +1,5 @@
 avd_peco(){
-  if ! type android 2>/dev/null 1>/dev/null
+  if ! type avdmanager 2>/dev/null 1>/dev/null
   then
     echo -e 'Error: android command not found.\nYou must install Android SDK.\nhttps://developer.android.com/sdk/index.html'
   fi
@@ -9,7 +9,7 @@ avd_peco(){
     echo -e 'Error: peco command not found.\nYou must install peco.\nhttps://github.com/peco/peco'
   fi
 
-  avd_name=`android list avds | grep -oe "Name: [a-zA-Z0-9_]*" | sed -e 's/Name: //' | peco`
+  avd_name=`avdmanager list avds | grep -oe "Name: [a-zA-Z0-9_]*" | sed -e 's/Name: //' | peco`
   if [[ $avd_name =~ ^(.+) ]] ; then
      echo "boot $avd_name $1"
      emulator -avd "$avd_name" $1 &
